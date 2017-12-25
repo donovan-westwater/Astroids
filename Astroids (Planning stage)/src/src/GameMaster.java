75a37b2369a87bb411c41755871ca74c15fc9403
donovan-westwater <donovan.westwater@gmail.com>
2017-12-25 13:09:46 -0500

    Copied current progress into folder

---
+++ Astroids (Planning stage)/src/src/GameMaster.java
@@ -0,0 +1,112 @@
+import java.util.ArrayList;
+
+import edu.princeton.cs.algs4.StdAudio;
+import edu.princeton.cs.algs4.StdDraw;
+import java.awt.Font;
+
+public class GameMaster {
+	private GfxUI screen;
+	private GfxUI replaytext;
+	private GfxUI scoretext;
+	private GameEngine eng;
+	private int score;
+	public enum Phase {
+		PREGAME, GAME, POSTGAME;
+	}
+	private int phase = 1;
+	private int press_amount = 0;
+	private ArrayList<GameObject> phase1;
+	private ArrayList<GameObject> phase2;
+	private ArrayList<GameObject> phase3;
+	private ArrayList<GameObject> cities;
+	
+	public GameMaster(GameEngine tim){
+		screen = new GfxUI(new Vec2d(0,0));
+		replaytext = new GfxUI(new Vec2d(0, -1.0));
+		scoretext = new GfxUI(new Vec2d(-2.5, 4.5));
+		eng = tim;
+		eng.setGameMaster(this);
+		this.startPhase1();
+		replaytext.setMsg("");
+		scoretext.setMsg("");
+	}
+	public void update(){
+		if(StdDraw.isKeyPressed(27) && press_amount == 0){
+			changePhase(2);
+			press_amount += 1;
+			this.startPhase2();
+		}
+		boolean anyCitiesLeft = false;
+		
+		
+		if (phase == 1) {
+			screen.setMsg("MISSLE COMMAND!: PRESS 'ESCAPE' TO START!");
+		}
+		if (phase == 2) {
+			StdDraw.setFont();
+			scoretext.setMsg("SCORE: " + score);
+		}
+		if (!anyCitiesLeft && phase == 2) {
+			this.startPhase3();
+			this.changePhase(3);
+		}
+		if (phase == 3) {
+			if(StdDraw.isKeyPressed(88)) {
+				replaytext.setMsg("");
+				startPhase2();
+			}
+		}
+	}
+	private void startPhase1(){
+		screen.setCenter(new Vec2d(0,0));
+		eng.addUI(screen);
+		eng.addUI(replaytext);
+		eng.addUI(scoretext);
+	}
+	private void startPhase2(){
+		this.changePhase(2);
+		double minX = -5.0;
+		double minY = -5.0;
+		double maxX = 5.0;
+		double maxY = 5.0;
+		screen.setMsg("");
+		scoretext.setMsg("SCORE: " + score);
+
+		Player hansolo = new Player(eng);
+		eng.setPlayer(hansolo);
+
+		Vec2d[] targets = new Vec2d[9];
+		double gutter = 1.0;
+		double maxWidth = Math.abs(maxX - minX);
+		double targetWidth = maxWidth - gutter;
+		double defaultY = -4.0;
+		System.out.println("Gutter: " + gutter + " and targetWidth " + targetWidth );
+
+		for (int i = 0; i < targets.length; i++) {
+			targets[i] = new Vec2d(minX + gutter/2 + targetWidth * i / (targets.length-1), defaultY);
+		}
+		// first object into system draws first
+
+
+		PhyBox b1 = new PhyBox(new Vec2d(0.0, 0.0), targets[0], 0.5, 10000000.0);
+		PhyBox b2 = new PhyBox(new Vec2d(0.0, 0.0), targets[4], 0.5, 20000000.0);
+		PhyBox b3 = new PhyBox(new Vec2d(0.0, 0.0), targets[8], 0.5, 1000000000.0);
+		
+		
+	}
+	private void startPhase3(){
+		screen.setMsg("GAME OVER");
+		replaytext.setMsg("");
+		eng.killPlayer();
+
+	}
+	private void changePhase(int num){
+		phase = num;
+	}
+	public int getPhase(){
+		return phase;
+	}
+	public void addScore(int pts) {
+		this.score = this.score + pts;
+	}
+}
---
+++ Astroids (Planning stage)/src/src/PlayerMissle.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+
+import edu.princeton.cs.algs4.StdDraw;
+
+public class PlayerMissle extends GameObject{
+	private Vec2d mytarget;
+	private double triggerDist = 0.1;
+	
+	private PhyBox p;
+	private GfxObject g;
+	private GameEngine gE;
+	
+	
+	public PlayerMissle(GfxObject gfx, PhyBox phy, GameEngine gEngine, Vec2d target) {
+		super(gfx, phy, gEngine);
+		this.mytarget = target;
+		p = phy;
+		g = gfx;
+		gE = gEngine;
+		
+	}
+	//This should be a straight line, coming off the space ship (Player)
+
+	public void update () {
+		// we don't need to move, cause phyeng does that
+		// we do need to add any fx that we want
+		
+		//gE.addDuringFrame(t); #Use this for animation/ updating graphical postion of missle
+
+		// we need to see if we reached destination
+		// we need to see if we hit some something first
+		Vec2d currentPosition = this.getPhysicsPos();
+		Vec2d subbedVec = Vec2d.subtract(currentPosition, mytarget);
+		
+		double distSqrd = Vec2d.dotAB(subbedVec, subbedVec);
+		if (distSqrd < (triggerDist*triggerDist)) {
+			this.remove();
+			gE.removeDuringFrame(this);
+			System.out.println("Hey, I went boom!");
+			gE.lowerMissileCount();
+		}
+	}
+	public void remove() {
+		
+	}
+
+	public static void main(String[] args){
+		double minX = -5.0;
+		double minY = -5.0;
+		double maxX = 5.0;
+		double maxY = 5.0;
+		GameEngine gEngine = new GameEngine(minX, minY, maxX, maxY);
+		
+		PhyBox enemy = new PhyBox(new Vec2d(0.1,-0.1), new Vec2d(-4,0), 0.2, 10);
+		GfxCircle gfxEnemy = new GfxCircle(0.2);
+		
+		PhyBox enemy2 = new PhyBox(new Vec2d(0.1,-0.1), new Vec2d(-3,0), 0.2, 10);
+		GfxCircle gfxEnemy2 = new GfxCircle(0.2);
+		
+		PhyBox city1 = new PhyBox(new Vec2d(0.0, 0.0), new Vec2d(-4.0, -4.0), 0.4, 1000000);
+	
+		// not really kosher, should be part of game obj system
+		
+		while(true) {
+			gEngine.update();
+			
+		}
+	}
+}
