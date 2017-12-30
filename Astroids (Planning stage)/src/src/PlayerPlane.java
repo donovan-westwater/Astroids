fe423569859c49c3e0e9cf3f527e3e204b93aa54
donovan-westwater <donovan.westwater@gmail.com>
2017-12-29 19:48:48 -0500

    Made some head way
    Not getting very far

--- Astroids (Planning stage)/src/src/PlayerPlane.java
+++ Astroids (Planning stage)/src/src/PlayerPlane.java
@@ -1,13 +1,13 @@
 import java.lang.*;
 public class PlayerPlane extends GameObject{
 	
-	private static GfxBox sprite = new GfxBox(1);
+	private static GfxCircle sprite = new GfxCircle(1);
 	private Vec2d spawn = new Vec2d(0,0);
 	private Vec2d movement = new Vec2d(0,0);
 	private static PhyBox hitBox = new PhyBox(new Vec2d(0.25,0),new Vec2d(1,-1),1,0);
 	public PlayerPlane(GameEngine gEng) {
 		super(sprite,hitBox,gEng);
-		sprite = (GfxBox)this.getGfxObj();
+		sprite = (GfxCircle)this.getGfxObj();
 		
 		// TODO Auto-generated constructor stub
 	}
